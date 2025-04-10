84a89b707b066a36c99d815db09b0af05700b7ac
Israr Ahmad <154470627+israrahmad831@users.noreply.github.com>
2025-04-11 02:06:29 +0500

    added navigation to main page from tournaments page

--- src/pages/Tournament/Tournament.jsx
+++ src/pages/Tournament/Tournament.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./Tournament.module.css";
+import { useNavigate } from "react-router-dom";
+
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 const Tournament = () => {
   const [data, setData] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchTournaments();
@@ -11,6 +14,9 @@ const Tournament = () => {
   const fetchTournaments = async () => {
     const response = await axios.get(`${API_BASE_URL}/api/tournaments`);
     setData(response.data);
+  };
+  const handleRowClick = (id) => {
+    navigate(`/${id}`);
   };
   return (
     <div className={styles.table}>
@@ -25,7 +31,11 @@ const Tournament = () => {
         </thead>
         <tbody>
           {data.map((tournament, index) => (
-            <tr key={tournament._id}>
+            <tr
+              key={tournament._id}
+              onClick={() => handleRowClick(tournament._id)}
+              style={{ cursor: "pointer" }}
+            >
               <td>{index + 1}</td>
               <td>{tournament.name}</td>
               <td>{tournament.startDate}</td>
